a3bf342edebb4ff6dd03fe1090c270abb52d4f4c
sandy <sandy.ali.basha@gmail.com>
2024-11-02 17:07:37 +0300

    update

---
+++ src/modules/product/hooks/useProductsIndex.js
@@ -0,0 +1,141 @@
+import React, { useMemo, useCallback, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useTranslation } from "react-i18next";
+import { colorStore } from "store/ColorsStore";
+import { useProduct } from "hooks/product/useProduct";
+
+export const useProductIndex = () => {
+  const { t } = useTranslation("index");
+  const { data, isLoading } = useProduct();
+  const navigate = useNavigate();
+  const [id, setID] = useState();
+  const [editedID, setEditedID] = colorStore((state) => [
+    state.editedID,
+    state.setEditedID,
+  ]);
+
+  // States for sorting and filtering
+
+  const [cityFilter, setCityFilter] = useState("");
+  const [brandFilter, setBrandFilter] = useState("");
+
+  const columns = useMemo(() => {
+    return [
+      t(""),
+      t("name"),
+      t("brand"),
+      t("sku"),
+      t("price"),
+      t("price before sale"),
+      t("Qty"),
+      t("city"),
+      t("status"),
+      t("option"),
+    ];
+  }, [t]);
+  const handleCreate = () => navigate("create");
+
+  const [OpenDelete, setOpenDelete] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [openAttr, setOpenAttr] = useState(false);
+  const [openImagesSlider, setOpenImagesSlider] = useState(false);
+  const [product_attr, setProduct_attr] = useState();
+  const [checkedIDs, setCheckedIDs] = useState([]); // New state for checked IDs
+  // Pagination state
+  const [page, setPage] = useState(0);
+
+  const [pageSize, setPageSize] = useState(10);
+
+  const handleView = useCallback(
+    (id) => {
+      navigate("view/" + id);
+    },
+    [navigate]
+  );
+  const handleEdit = useCallback(
+    (id) => {
+      setEditedID(id);
+    },
+    [setEditedID]
+  );
+  const handleAddImages = useCallback((id) => {
+    setID(id);
+    setOpen(true);
+  }, []);
+  const handleDelete = useCallback((id) => {
+    setID(id);
+    setOpenDelete(true);
+  }, []);
+  const handleImagesSlider = useCallback((id) => {
+    setID(id);
+    setOpenImagesSlider(true);
+  }, []);
+
+  const handleCat = useCallback((id, attr) => {
+    setID(id);
+    setOpenAttr(true);
+    setProduct_attr(attr);
+  }, []);
+
+  // Sort and filter logic
+  const filteredData = useMemo(() => {
+    let filtered = data?.data?.products || [];
+
+    if (cityFilter) {
+      filtered = filtered.filter((product) =>
+        product.cities?.state[0]?.name
+          ?.toLowerCase()
+          .includes(cityFilter.toLowerCase())
+      );
+    }
+
+    if (brandFilter) {
+      filtered = filtered.filter((product) =>
+        product.brand?.name?.toLowerCase().includes(brandFilter.toLowerCase())
+      );
+    }
+
+    return filtered;
+  }, [data, cityFilter, brandFilter]);
+
+  // Function to handle checkbox changes
+  const handleCheckboxChange = (id) => {
+    setCheckedIDs((prevChecked) =>
+      prevChecked.includes(id)
+        ? prevChecked.filter((checkedID) => checkedID !== id)
+        : [...prevChecked, id]
+    );
+  };
+
+  return {
+    handleCheckboxChange,
+    handleDelete,
+    handleImagesSlider,
+    handleCat,
+    handleCreate,
+    OpenDelete,
+    open,
+    openAttr,
+    openImagesSlider,
+    product_attr,
+    handleView,
+    handleEdit,
+    handleAddImages,
+    isLoading,
+    id,
+    editedID,
+    setCityFilter,
+    setBrandFilter,
+    columns,
+    checkedIDs,
+    navigate,
+    setOpen,
+    setOpenImagesSlider,
+    setOpenDelete,
+    setOpenAttr,
+    cityFilter,
+    brandFilter,
+    filteredData,
+    t,
+  };
+};
--- src/modules/product/pages/ProductIndex.jsx
+++ src/modules/product/pages/ProductIndex.jsx
@@ -1,186 +1,145 @@
-import {
-  Typography,
-  Box,
-  TableRow,
-  TableCell,
-  Button,
-  TextField,
-} from "@mui/material";
-import { BoxStyled } from "components/styled/BoxStyled";
-import React, { useMemo, useCallback, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-import { Table } from "components/shared";
+import React, { useState } from "react";
+import { Typography, Box, Button, TextField, Chip } from "@mui/material";
 import Loader from "components/shared/Loader";
-import { colorStore } from "store/ColorsStore";
-import { useProduct } from "hooks/product/useProduct";
 import ProductUpdate from "./ProductUpdate";
 import DeleteDialog from "../components/Dialog";
 import AddImages from "./AddImages";
 import ProductAttr from "./ProductAttr";
 import AddImagesSlider from "./AddImagesSlider";
-//* icons
 import AddRoundedIcon from "@mui/icons-material/AddRounded";
 import ChangeStatus from "../components/ChangeStatus";
 import ProductMenu from "../components/productMenu";
+import { useProductIndex } from "../hooks/useProductsIndex";
+import { DataGrid } from "@mui/x-data-grid";
 
 const ProductIndex = () => {
-  const { t } = useTranslation("index");
-  const { data, isLoading } = useProduct();
-  const navigate = useNavigate();
-  const [id, setID] = useState();
-  const [editedID, setEditedID] = colorStore((state) => [
-    state.editedID,
-    state.setEditedID,
-  ]);
-
-  // States for sorting and filtering
-
-  const [cityFilter, setCityFilter] = useState("");
-  const [brandFilter, setBrandFilter] = useState("");
-
-  const columns = useMemo(() => {
-    return [
-      t("name"),
-      t("brand"),
-      t("sku"),
-      t("price"),
-      t("price before sale"),
-      t("Qty"),
-      t("city"),
-      t("status"),
-      t("option"),
-    ];
-  }, [t]);
-  const handleCreate = () => navigate("create");
-
-  const [OpenDelete, setOpenDelete] = useState(false);
-  const [open, setOpen] = useState(false);
-  const [openAttr, setOpenAttr] = useState(false);
-  const [openImagesSlider, setOpenImagesSlider] = useState(false);
-  const [product_attr, setProduct_attr] = useState();
-
-  const handleView = useCallback(
-    (id) => {
-      navigate("view/" + id);
-    },
-    [navigate]
-  );
-  const handleEdit = useCallback(
-    (id) => {
-      setEditedID(id);
-    },
-    [setEditedID]
-  );
-  const handleAddImages = useCallback((id) => {
-    setID(id);
-    setOpen(true);
-  }, []);
-  const handleDelete = useCallback((id) => {
-    setID(id);
-    setOpenDelete(true);
-  }, []);
-  const handleImagesSlider = useCallback((id) => {
-    setID(id);
-    setOpenImagesSlider(true);
-  }, []);
-
-  const handleCat = useCallback((id, attr) => {
-    setID(id);
-    setOpenAttr(true);
-    setProduct_attr(attr);
-  }, []);
-
-  // Sort and filter logic
-  const filteredData = useMemo(() => {
-    let filtered = data?.data?.products || [];
-
-    if (cityFilter) {
-      filtered = filtered.filter((product) =>
-        product.cities?.state[0]?.name
-          ?.toLowerCase()
-          .includes(cityFilter.toLowerCase())
-      );
-    }
-
-    if (brandFilter) {
-      filtered = filtered.filter((product) =>
-        product.brand?.name?.toLowerCase().includes(brandFilter.toLowerCase())
-      );
-    }
-
-    return filtered;
-  }, [data, cityFilter, brandFilter]);
-
-  const rows = useMemo(() => {
-    return filteredData?.map((product, id) => (
-      <TableRow sx={{ height: "65px" }} key={product.id}>
-        <TableCell sx={{ minWidth: 250 }}>{product?.name ?? "Null"}</TableCell>
-        <TableCell sx={{ minWidth: 50 }}>
-          {product?.brand?.name ?? "Null"}
-        </TableCell>
-        <TableCell sx={{ minWidth: 150 }}>{product?.sku ?? "Null"}</TableCell>
-        <TableCell sx={{ minWidth: 50 }}>{product?.price ?? "Null"}</TableCell>
-        <TableCell sx={{ minWidth: 100 }}>
-          {product?.compare_price > 0 ? (
-            <Box
-              sx={{
-                border: `1px solid`,
-                color: "success.main",
-                borderRadius: 3,
-              }}
-            >
-              {product?.compare_price}
-            </Box>
-          ) : (
-            "no sale"
-          )}
-        </TableCell>
-        <TableCell sx={{ minWidth: 50 }}>
-          {product?.quantity ?? "Null"}
-        </TableCell>
-        <TableCell sx={{ minWidth: 50 }}>
-          {product?.cities?.state[0]?.name ?? "Null"}
-        </TableCell>
-        <TableCell sx={{ minWidth: 50 }} align="center">
-          <ChangeStatus
-            id={product.id}
-            action={product.status === "active" && "change-status"}
-          >
-            {product.status}
-          </ChangeStatus>
-        </TableCell>
-        <TableCell align="center" sx={{ minWidth: 50 }}>
-          <ProductMenu
-            product={product}
-            count={product.length}
-            page={1}
-            handleEdit={handleEdit}
-            handleView={handleView}
-            handleAddImages={handleAddImages}
-            handleImagesSlider={handleImagesSlider}
-            handleDelete={handleDelete}
-            handleCat={handleCat}
-            navigate={navigate}
-          />
-        </TableCell>
-      </TableRow>
-    ));
-  }, [
-    filteredData,
-    handleEdit,
-    handleView,
-    handleAddImages,
-    handleImagesSlider,
+  const {
     handleDelete,
+    handleImagesSlider,
     handleCat,
+    handleCreate,
+    OpenDelete,
+    open,
+    openAttr,
+    openImagesSlider,
+    product_attr,
+    handleView,
+    handleEdit,
+    handleAddImages,
+    isLoading,
+    id,
+    editedID,
+    setCityFilter,
+    setBrandFilter,
     navigate,
-  ]);
+    setOpen,
+    setOpenImagesSlider,
+    setOpenDelete,
+    setOpenAttr,
+    cityFilter,
+    brandFilter,
+    filteredData,
+    t,
+  } = useProductIndex();
+
+  // Prepare the rows for the DataGrid
+  const rows = filteredData.map((product) => ({
+    id: product.id,
+    name: product.name ?? "Null",
+    brand: product.brand?.name ?? "Null",
+    sku: product.sku ?? "Null",
+    price: product.price ?? "Null",
+    comparePrice: product.compare_price > 0 ? product.compare_price : "no sale",
+    quantity: product.quantity ?? "Null",
+    city: product.cities?.state[0]?.name ?? "Null",
+    status: product.status,
+    actions: product, // Passing product object for actions
+  }));
+
+  // Define columns for the DataGridPremium
+  const gridColumns = [
+    {
+      field: "name",
+      headerName: "Product Name",
+      width: 250,
+    },
+    {
+      field: "brand",
+      headerName: "Brand",
+      width: 100,
+    },
+    {
+      field: "sku",
+      headerName: "SKU",
+      width: 100,
+    },
+    {
+      field: "price",
+      headerName: "Price",
+      width: 100,
+    },
+    {
+      field: "comparePrice",
+      headerName: "Compare Price",
+      width: 150,
+      renderCell: (params) => <Chip label={params.value}>{params.value}</Chip>,
+    },
+    {
+      field: "quantity",
+      headerName: "Quantity",
+      width: 100,
+    },
+    {
+      field: "city",
+      headerName: "City",
+      width: 100,
+    },
+    {
+      field: "status",
+      headerName: "Status",
+      width: 100,
+      renderCell: (params) => (
+        <ChangeStatus
+          id={params.row.id}
+          action={params.row.status === "active" && "change-status"}
+        >
+          {params.row.status}
+        </ChangeStatus>
+      ),
+    },
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 150,
+      renderCell: (params) => (
+        <ProductMenu
+          product={params.value}
+          count={params.value ? params.value.length : 0} // Add fallback for undefined values
+          page={1}
+          handleEdit={handleEdit}
+          handleView={handleView}
+          handleAddImages={handleAddImages}
+          handleImagesSlider={handleImagesSlider}
+          handleDelete={handleDelete}
+          handleCat={handleCat}
+          navigate={navigate}
+        />
+      ),
+    },
+  ];
+
+  const [selectedRowIds, setSelectedRowIds] = useState([]);
+  // Capture selected row IDs
+  const handleSelectionChange = (selectionModel) => {
+    setSelectedRowIds(selectionModel);
+  };
+
+  console.log(selectedRowIds);
 
   return isLoading ? (
     <Loader />
   ) : (
-    <>
+    <Box sx={{ overflow: "scroll" }}>
       {editedID && <ProductUpdate id={editedID} />}
       {id && <AddImages id={id} open={open} setOpen={setOpen} />}
       {id && (
@@ -202,9 +161,9 @@ const ProductIndex = () => {
 
       <Box
         sx={{
-          width: { sl: "300px" },
-          backgroundColor: { xs: "background.main" },
-          ml: { xs: "0px" },
+          width: "100%",
+          backgroundColor: "background.main",
+          p: 3,
         }}
       >
         <Box
@@ -212,10 +171,10 @@ const ProductIndex = () => {
             display: "flex",
             justifyContent: "space-between",
             alignItems: "center",
-            mb: "25px",
+            mb: 2,
           }}
         >
-          <Typography sx={{ color: "text.main" }} variant="h5">
+          <Typography variant="h5" sx={{ color: "text.main" }}>
             {t("product")}
           </Typography>
           <Button
@@ -227,13 +186,11 @@ const ProductIndex = () => {
             {t("New product")}
           </Button>
         </Box>
-
-        <Box sx={{ mb: "20px" }}>
+        <Box sx={{ display: "flex", gap: 2, mb: 3 }}>
           <TextField
             label={t("City filter")}
             value={cityFilter}
             onChange={(e) => setCityFilter(e.target.value)}
-            sx={{ mr: 2 }}
           />
           <TextField
             label={t("Brand filter")}
@@ -241,15 +198,22 @@ const ProductIndex = () => {
             onChange={(e) => setBrandFilter(e.target.value)}
           />
         </Box>
-        <BoxStyled>
-          <Table
-            columns={columns}
+
+        <Box sx={{ width: "70vw", overflow: "scroll" }}>
+          <DataGrid
             rows={rows}
-            sx={{ border: "1px solid", borderColor: "secondary.main" }}
+            columns={gridColumns}
+            pagination
+            checkboxSelection
+            disableSelectionOnClick
+            sx={{
+              backgroundColor: "background.paper",
+            }}
+            onSelectionModelChange={handleSelectionChange} // Capture selected row IDs
           />
-        </BoxStyled>
+        </Box>
       </Box>
-    </>
+    </Box>
   );
 };
 
